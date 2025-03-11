2fb701fbe47461b6f3f315c57e4abb73144d1b87
xzadudu179 <terra179@163.com>
2025-03-11 21:52:11 +0800

    init files

---
+++ CF4A/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Watermelon;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Watermelon
+        int w = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i < w; i++)
+        {
+            Console.WriteLine($"{i} {w - i}");
+            if (i % 2 != 0 || (w - i) % 2 != 0) continue;
+            Console.WriteLine("YES");
+            return;
+        }
+        Console.WriteLine("NO");
+    }
+}
